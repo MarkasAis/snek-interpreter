7df7961a7ee77ecf0bd08f8bfb641e13f5a0d9d3
MarkAis <mark.aisparas@gmail.com>
2025-02-27 15:24:58 +0200

    Added indentation parsing

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "snek/lexer"
+import (
+	"snek/lexer"
+	"snek/parser"
+)
 
 func main() {
-	code := `def foo():
-    x = 42 # comments!!!
+	// code := `def foo():
+	// x = 42 # comments!!!
 
-    if x > 0:
-        print("Hello")`
+	// if x > 0:
+	//     print("Hello")`
+
+	code := `
+pass
+pass
+pass
+`
+
+	lexer0 := lexer.NewLexer(code)
+	lexer0.PrintTokens()
 
 	lexer := lexer.NewLexer(code)
-	lexer.PrintTokens()
+	// lexer.PrintTokens()
+
+	parser := parser.New(lexer)
+	parser.ParseProgram()
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,8 +1,10 @@
 package parser
 
 import (
+	"fmt"
 	"snek/lexer"
 	"snek/token"
+	"strings"
 )
 
 type Parser struct {
@@ -12,6 +14,113 @@ type Parser struct {
 	errors    []string
 }
 
-func (p *Parser) ParseProgram() {
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) ParseProgram() {
+	p.parseBlock(-1)
+}
+
+func (p *Parser) parseBlock(prevIndentation int) {
+	startIndentation := p.getIndentation()
+
+	if startIndentation <= prevIndentation {
+		fmt.Print("not enough indentation")
+		return
+	}
+
+	statements := []int{}
+
+	curIndentation := startIndentation
+	for curIndentation == startIndentation {
+		p.nextToken()
+		stmt := p.parseStatement()
+		// if stmt != nil {
+		statements = append(statements, stmt)
+		// }
+
+		if !p.curTokenIs(token.NEW_LINE) {
+			break
+		}
+		curIndentation = p.getIndentation()
+	}
+
+	if curIndentation > startIndentation {
+		fmt.Printf("too much indentation [%d, %d]", curIndentation, startIndentation)
+	}
+
+	if len(statements) == 0 {
+		fmt.Print("no statements")
+	}
+
+	fmt.Print(len(statements))
+}
+
+func (p *Parser) parseStatement() int {
+	for !p.curTokenIs(token.NEW_LINE) && !p.curTokenIs(token.EOF) {
+		p.nextToken()
+	}
+	return 1
+}
+
+func (p *Parser) getIndentation() int {
+	if p.curToken.Type != token.NEW_LINE {
+		fmt.Print("expected new line")
+		return -1
+	}
+
+	for p.peekTokenIs(token.NEW_LINE) {
+		p.nextToken()
+	}
+
+	spaceCount := strings.Count(p.curToken.Literal, " ")
+	tabCount := strings.Count(p.curToken.Literal, "\t")
+
+	if spaceCount > 0 && tabCount > 0 {
+		fmt.Print("mixed indentation")
+		return -1
+	}
+
+	return spaceCount + tabCount
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) curError(t token.TokenType) {
+	msg := fmt.Sprintf("expected token to be %s, got %s instead", t, p.curToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
